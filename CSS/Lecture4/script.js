1614d416e59aac83782798169a44e6e94fd8e58d
imamhossenbu <imam62310@gmail.com>
2025-04-22 12:05:14 +0600

    first

---
+++ CSS/Lecture4/script.js
@@ -0,0 +1,17 @@
+gsap.from("ul li", {
+    duration: 2,
+    x: -100,
+    opacity: 0,
+    stagger: 0.2,
+})
+
+
+
+
+
+gsap.from("h1", {
+    duration: 2,
+    y: -100,
+    opacity: 0,
+    stagger: 0.2,
+})
